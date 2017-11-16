df1c74ce01ddde50a089f2bc23b03a0eba8e23e8
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2017-11-16 09:38:05 -0200

    EventEmitter com ngDestroy

--- app1/src/app/app.component.ts
+++ app1/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  public jogoEmAndamento: boolean = true
+
+  public encerrarJogo(tipo: string): void {
+    console.log(tipo);
+    this.jogoEmAndamento = false
+  }
 }
--- app1/src/app/painel/painel.component.ts
+++ app1/src/app/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
 
 import { Frase } from '../shared/frase.model';
 import { FRASES } from './frases-mock';
@@ -8,7 +8,7 @@ import { FRASES } from './frases-mock';
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.css']
 })
-export class PainelComponent implements OnInit {
+export class PainelComponent implements OnInit, OnDestroy {
 
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase:'
@@ -21,11 +21,18 @@ export class PainelComponent implements OnInit {
 
   public tentativas: number = 3
 
+  @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter()
+
   constructor() {
     this.atualizaRodada()
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    console.log('Componente painel foi destruido!');
+
   }
 
   public atualizaResposta(resposta: Event): void {
@@ -45,7 +52,8 @@ export class PainelComponent implements OnInit {
 
       // Conclusao das traducoes
       if (this.rodada === 4) {
-        alert('Concluiu as traducoes com sucesso!')
+        //alert('Concluiu as traducoes com sucesso!')
+        this.encerrarJogo.emit('Vitória')
       }
 
       // atualiza o objeto rodadaFrase
@@ -56,7 +64,8 @@ export class PainelComponent implements OnInit {
       this.tentativas--
 
       if (this.tentativas === -1) {
-        alert('Voce perdeu todas as tentativas')
+        //alert('Voce perdeu todas as tentativas')
+        this.encerrarJogo.emit('Derrota')
       }
     }
 
